e492ea7d9e5dc4d63430ffc225e7876d15238722
Liam Ansoumane Kande <kande.ans@outlook.com>
2017-08-14 07:08:05 -0400

    Re-created the entire UI interface with ReactJs

--- components/js/app.js
+++ components/js/app.js
@@ -11,7 +11,8 @@ var MainInterface = React.createClass({
           "downloadLink": "https://s3.us-east-2.amazonaws.com/liamkande.com/projects/Bulletin-Board-App-with-ReactJS.zip",
           "description": "Developed a Bulletin board app using React components and properties, added state to components to handle events, worked with PropTypes, developed a more complete app with React.js and companion tools like Babel, NPM, and more.",
           "type": "React",
-          "boxClass": "col-sm-12 col-md-12 box-sp boxwrapper"
+          "boxClass": "col-sm-12 col-md-12 box-sp boxwrapper",
+          "pClass": "text-center"
         },
         {
           "name": "Responsive Website & Images with JavaScript",
@@ -19,7 +20,8 @@ var MainInterface = React.createClass({
           "downloadLink": "https://s3.us-east-2.amazonaws.com/liamkande.com/projects/Website_Images_with_JS.zip",
           "description": "Created a fully functional website using JavaScript. Automated responsive images markup, and worked with objects, variables, data types, conditionals, functions, loops, closures, the DOM and more.",
           "type": "JavaScript",
-          "boxClass": "col-sm-12 col-md-12 box-sp-two boxwrapper"
+          "boxClass": "col-sm-12 col-md-12 box-sp-two boxwrapper",
+          "pClass": "text-center"
         },
         {
           "name": "Responsive Design with Bootstrap",
@@ -27,10 +29,73 @@ var MainInterface = React.createClass({
           "downloadLink": "https://s3.us-east-2.amazonaws.com/liamkande.com/projects/Responsive_Design_with_Bootstrap.zip",
           "description": "Created a website with a custom Bootstrap layout grid, Inserted a navigation bar, a container for a hero image, glyphicons and page content by working with columns and containers.",
           "type": "Bootstrap",
-          "boxClass": "col-sm-12 col-md-12 box-sp-three boxwrapper"
+          "boxClass": "col-sm-12 col-md-12 box-sp-three boxwrapper",
+          "pClass": "text-center"
         },
-
-      ]
+        {
+          "name": "Interactive Resume",
+          "skills": "jQuery | JavaScript | JSON | HTML | CSS",
+          "downloadLink": "https://s3.us-east-2.amazonaws.com/liamkande.com/projects/interactive_Resume.zip",
+          "description": "Using jQuery, I developed an interactive resume application that reads all data from a JSON file and then dynamically modifies the DOM to display the information.",
+          "type": "jQuery",
+          "boxClass": "col-sm-6 col-md-4 box-one boxwrapper",
+          "pClass": "text-center detail"
+        },
+        {
+          "name": "SEO Audit",
+          "skills": "SEO",
+          "downloadLink": "http://liamkande.com/projects/P4_-_SEO_Audit__Project_Template_Slides.pdf",
+          "description": "Performed an audit of a website, recommended actions to improve the website's ranking in search engine results, created a target keyword list, evaluated the design, content and the UX of the site, and recommended improvements.",
+          "type": "SEO",
+          "boxClass": "col-sm-6 col-md-4 box-two boxwrapper",
+          "pClass": "text-center detail"
+        },
+        {
+          "name": "Classic Arcade Game Clone",
+          "skills": "JavaScript | Object-Oriented Programming | HTML5 Canvas",
+          "downloadLink": "https://s3.us-east-2.amazonaws.com/liamkande.com/projects/Arcade-game_Frogger-Clone.zip",
+          "description": "An HTML5 Canvas powered video game, developed using the best practices in Object Oriented JavaScript.",
+          "type": "JavaScript",
+          "boxClass": "col-sm-6 col-md-4 box-three boxwrapper",
+          "pClass": "text-center detail"
+        },
+        {
+          "name": "Website Optimization",
+          "skills": "HTML | CSS | Chrome Developer Tools | critical rendering path | 60FPS rendering",
+          "downloadLink": "https://s3.us-east-2.amazonaws.com/liamkande.com/projects/Optimization3.zip",
+          "description": "Optimized an inefficient web application's JavaScript, CSS and assets delivery, ensuring it runs at 60fps and achieves a PageSpeed score of at least 90.",
+          "type": "Optimization",
+          "boxClass": "col-sm-6 col-md-4 box-four boxwrapper",
+          "pClass": "text-center detail"
+        },
+        {
+          "name": "Typing Speed Tester",
+          "skills": "JavaScript | CSS | HTML",
+          "downloadLink": "https://s3.us-east-2.amazonaws.com/liamkande.com/projects/Typing_Speed_Tester.zip",
+          "description": "Used JavaScript to interact with the components of a app by creating event listeners to detect typing in the text area. Built a count-up timer. Created functions for leading zeros, spelling errors detector, and a reset button to stop the timer when the test is done.",
+          "type": "JavaScript",
+          "boxClass": "col-sm-6 col-md-4 box-three boxwrapper",
+          "pClass": "text-center detail"
+        },
+        {
+          "name": "Run an AdWords Campaign",
+          "skills": "Search advertising with Adwords",
+          "downloadLink": "http://liamkande.com/projects/P5_-Part_2.pdf",
+          "description": "Created, executed, and monitored a search engine marketing campaign on AdWords for a product. Evaluated post-campaign results to and drafted a strategy for additional campaigns focused on ROI.",
+          "type": "AdWords",
+          "boxClass": "col-sm-6 col-md-4 box-two boxwrapper",
+          "pClass": "text-center detail"
+        },
+        {
+          "name": "Analog clock",
+          "skills": "JavaScript | CSS | HTML",
+          "downloadLink": "https://s3.us-east-2.amazonaws.com/liamkande.com/projects/Analog_Clock_Project.zip",
+          "description": "Used objects and methods to get the current time, variables to hold information, methods to break that time, operators to translate information into degrees and DOM element properties for positioning.",
+          "type": "JavaScript",
+          "boxClass": "col-sm-12 col-md-12 box-sp-two boxwrapper",
+          "pClass": "text-center"
+        }
+      ]//Json data
     } //return
   }, //getInitialState
 
@@ -42,11 +107,15 @@ var MainInterface = React.createClass({
         images = "/images/thumbnail-SVG/thumbnail-4.svg";
       } else if (images == "JavaScript") {
           images = "/images/thumbnail-SVG/thumbnail-3.svg";
-      } else if (images == "Bootstrap") {
+      } else if (images == "Bootstrap" || images == "Optimization") {
           images = "/images/thumbnail-SVG/thumbnail-1.svg";
-        }//images selection if statement
+        }else {
+          images = "/images/thumbnail-SVG/thumbnail-2.svg";
+        }
+        //images selection if statement
 
         var boxClasses = this.state.data[index].boxClass;
+        var pClasses = this.state.data[index].pClass;
 
       return (
         <div className={boxClasses} key={index}>
@@ -59,7 +128,7 @@ var MainInterface = React.createClass({
           <img src={"/images/Download-Glyph.svg"} width={"30"} />
           <h5>DOWNLOAD</h5>
           </a>
-          <p className="text-center">{this.state.data[index].description}</p>
+          <p className={pClasses}>{this.state.data[index].description}</p>
         </div>
       )//return
     }.bind(this));//filtered.map
